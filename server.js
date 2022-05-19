086576014b16bf4622bd1ffb2fe277e357a60904
Kimberly West <kdenton993@gmail.com>
2022-05-18 18:51:20 -0600

    added files for js, handlebars, utils, images, sql

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 // create variables
-var path = require('path')
-const express
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
