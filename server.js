abb44c6f5fab25411cc25e66a6ace9c180c95e68
Kimberly West <kdenton993@gmail.com>
2022-06-15 20:23:49 -0600

    fixing server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const path = require('path');
 
-const sequelize = require('./config/config');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-app.use(session(sess));
+app.use(session(session));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
