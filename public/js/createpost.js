ad1587aae334050089d721329e00aa560f90a0fd
Kimberly West <kdenton993@gmail.com>
2022-06-19 14:54:24 -0600

    creating add post functions

---
+++ public/js/createpost.js
@@ -0,0 +1,25 @@
+const CreatePostHandler = async function(event) {
+    event.preventDefault();
+  
+    const postTitle = document.querySelector('#title');
+    const postContent = document.querySelector('#post');
+  
+    const response = await fetch('/api/post', {
+      method: 'POST',
+      body: JSON.stringify({
+        postTitle: postTitle.value,
+        postContent:postContent.value,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to post');
+    }
+  };
+  
+  document
+    .querySelector('.create-post-form')
+    .addEventListener('submit', CreatePostHandler);
